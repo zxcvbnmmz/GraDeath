91dfcb0e308c5fab46fd5ce9b60a89249205fb2e
zxcvbnmmz <th.023102@gmail.com>
2014-10-03 14:29:51 +0900

    FirstCommit

---
+++ GraDeath/main.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+INT WINAPI WinMain(HINSTANCE inst, HINSTANCE, LPSTR, INT){
+#ifdef _DEBUG
+	//	::_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_ALLOC_MEM_DF);
+#endif
+
+	// aaaaa
+	// sadf;lk;sadlfk
+}
