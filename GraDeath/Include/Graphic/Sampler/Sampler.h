c7369966ec672bfca5a5edede326a41f5ec57612
zxcvbnmmz <th.023102@gmail.com>
2014-10-17 11:27:23 +0900

    サンプラーの改造

--- GraDeath/Include/Graphic/Sampler/Sampler.h
+++ GraDeath/Include/Graphic/Sampler/Sampler.h
@@ -1,22 +1,13 @@
 #ifndef _SAMPLER_H__
 #define _SAMPLER_H__
 
-struct ID3D10SamplerState;
-
-class Sampler{
-public:
-	Sampler();
-
+namespace Sampler{
 	void SetPointWrap(unsigned int slot = 0);
 	void SetPointClamp(unsigned int slot = 0);
 	void SetLinearWrap(unsigned int slot = 0);
 	void SetLinearClamp(unsigned int slot = 0);
 	void SetAnisotropicWrap(unsigned int slot = 0);
 	void SetAnisotropicClamp(unsigned int slot = 0);
-	
-private:
-	class Pimpl;
-	Pimpl* pimpl;
 };
 
 #endif	// end of Sampler
