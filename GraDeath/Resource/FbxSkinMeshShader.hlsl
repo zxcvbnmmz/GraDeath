faefeb4e546e06730abe6f154fb459fe45dc1bd4
Siarli <enter.528@gmail.com>
2014-10-20 10:01:00 +0900

    hlslとスキン

---
+++ GraDeath/Resource/FbxSkinMeshShader.hlsl
@@ -0,0 +1,130 @@
+//定義
+#define MAX_BONE_MATRICES 255
+
+//グローバル
+Texture2D texDecal: register(t0);
+SamplerState samLinear : register(s0);
+SamplerState toonLinear : register(s1);
+
+float4 m_LightDir = float4( 0, 1, 1, 1 );
+
+cbuffer globalBuffer
+{	
+	matrix world;	//	ワールド行列
+	matrix wvp;		//	ワールドから射影までの変換行列
+    matrix constBoneWorld[MAX_BONE_MATRICES];	//	ボーン配列
+};
+
+//スキニング後の頂点・法線が入る
+struct Skin
+{
+    float4 Pos;
+    float3 Norm;
+};
+
+//	バーテックスバッファーの入力
+struct VS_SkinIn
+{
+    float3 Pos	: POSITION;			//	座標
+    float3 Norm : NORMAL;			//	法線
+    float2 Tex	: TEXCOORD;			//	テクスチャ座標
+    uint4  Bones : BONE_INDEX;		//	頂点に対応したボーンのインデックス
+	float4 Weights : BONE_WEIGHT;	//	ボーンの重み
+};
+
+//ピクセルシェーダーの入力
+struct PS_SkinIn
+{
+    float4 Pos	: SV_Position;		//	座標
+    float3 Norm : NORMAL;			//	法線
+    float2 Tex	: TEXCOORD;			//	テクスチャ座標
+	float4 Color : COLOR0;			//	出力カラー
+};
+
+/**
+	ウェイトに応じて頂点に対する姿勢行列を取得
+ */
+Skin SkinVert( VS_SkinIn Input )
+{
+	Skin Output = (Skin)0;
+
+	float4 Pos = float4(Input.Pos,1);
+	Pos.x*=-1;
+    float3 Norm = Input.Norm;    
+
+    //	ボーン0
+    uint iBone = Input.Bones.x;
+    float fWeight = Input.Weights.x;
+    matrix m = constBoneWorld[ iBone ];
+    Output.Pos += fWeight * mul( Pos, m );
+    Output.Norm += fWeight * mul( Norm, (float3x3)m );    
+
+    //	ボーン1
+    iBone = Input.Bones.y;
+    fWeight = Input.Weights.y;
+    m = constBoneWorld[ iBone ];
+    Output.Pos += fWeight * mul( Pos, m );
+    Output.Norm += fWeight * mul( Norm, (float3x3)m );    
+
+    //	ボーン2
+    iBone = Input.Bones.z;
+    fWeight = Input.Weights.z;
+    m = constBoneWorld[ iBone ];
+    Output.Pos += fWeight * mul( Pos, m );
+    Output.Norm += fWeight * mul( Norm, (float3x3)m );    
+
+    //	ボーン3
+    iBone = Input.Bones.w;
+    fWeight = Input.Weights.w;
+    m = constBoneWorld[ iBone ];
+    Output.Pos += fWeight * mul( Pos, m );
+    Output.Norm += fWeight * mul( Norm, (float3x3)m );
+    
+    return Output;
+}
+
+/**
+	頂点シェーダー
+ */
+PS_SkinIn SkinMeshVS( VS_SkinIn input )
+{
+    PS_SkinIn output;
+    
+	//	取得した姿勢行列を頂点に掛け合わせる
+    Skin vSkinned = SkinVert( input );
+
+	//	行列変換を実行
+    output.Pos = mul( vSkinned.Pos, wvp );
+
+	//	法線の回転
+    output.Norm = normalize( mul( vSkinned.Norm, ( float3x3 )world ) );
+
+	//	テクスチャ座標取得
+    output.Tex = input.Tex;
+
+    return output;
+}
+
+//	ピクセルシェーダー
+float4 SkinMeshPS(PS_SkinIn input) : SV_Target
+{	
+	//float4 Out;
+//
+	//// ハーフランバート拡散証明によるライティング計算
+	//float p = dot( input.Norm, -m_LightDir.xyz );
+	//p = p * 0.5f + 0.5f;
+	////p = p * p;
+//
+	//// ************************************************************
+	//// トゥーン処理
+	//// ************************************************************
+	//float4 Col = texDecal.Sample( toonLinear, float2( p, 0.0f ) );
+//
+	//Out = Col * texDecal.Sample( samLinear, input.Tex );
+//
+	//return Out;
+
+    float4 TexDiffuse = texDecal.Sample( samLinear, input.Tex ); 
+
+	return TexDiffuse;
+}
---
+++ GraDeath/Resource/FbxStaticShader.hlsl
@@ -0,0 +1,63 @@
+// テクスチャステート
+Texture2D		texDecal: register( t0 );		// テクスチャ
+SamplerState	samLinear : register( s0 );		// サンプラー
+
+// グローバル変数群
+cbuffer globalbuffer
+{
+	matrix world;		//	ワールド行列
+	matrix wvp;			//	変換行列
+	float4 ambient;		//	アンビエント(未使用)
+	float4 diffuse;		//	ディフューズ
+	float4 supecular;	//	スペキュラ(未使用)
+	float4 light;		//	ライトベクトル
+};
+
+// バーテックスシェーダー出力構造体
+struct VS_OUTPUT
+{
+	float4	pos		: SV_POSITION;
+	float4	color	: COLOR0;
+	float2	uv		: TEXCOORD;
+};
+
+// バーテックスシェーダー
+VS_OUTPUT FbxStaticShaderVS( float4 pos : POSITION, float4 norm : NORMAL, float2 uv : TEXCOORD )
+{
+	VS_OUTPUT output = ( VS_OUTPUT )0;
+
+	// ライトのベクトル取得
+	float3 lightDir = -light;
+
+	// 法線をワールド行列で変換。x3にしているのは平行移動行列をなくすため
+	// 法線、ライトベクトルの内積取得　normalize:正規化
+	float3 normal	= normalize( mul( norm, ( float3x3 )world ) );
+	lightDir = normalize( lightDir );
+
+	// 座標変換
+	output.pos		= mul( pos, wvp );
+
+	// 法線、ライトベクトルの内積取得　normalize:正規化
+	//normal = normalize( normal );
+	//lightDir = normalize( lightDir );
+
+	// dot:内積
+
+	// 法線とライトベクトルの内積を取得 saturate:閾値０～１
+	float4 NL = saturate( dot( normal, lightDir ) );
+	NL.w = 1;
+
+	// ディフューズカラーと乗算
+	output.color = diffuse * NL;
+
+	// テクスチャー座標取得
+	output.uv = uv;
+
+	return output;
+}
+
+// ピクセルシェーダー
+float4 FbxStaticShaderPS( VS_OUTPUT input ) : SV_Target
+{
+	return texDecal.Sample( samLinear, input.uv );// * input.color;
+}
