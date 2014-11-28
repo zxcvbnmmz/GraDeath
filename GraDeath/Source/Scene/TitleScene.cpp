60f845eb99a1ad6ef9bb69c907a65c8b00a7de46
ivimeff <cavestory3212@gmail.com>
2014-11-28 17:19:01 +0900

    タイトル仮

--- GraDeath/Source/Scene/TitleScene.cpp
+++ GraDeath/Source/Scene/TitleScene.cpp
@@ -8,7 +8,9 @@
 
 #include "Input\Keyboard.h"
 
-Sprite sprite;
+Sprite sprite, sStart, sCredit, sExit, sVector;
+D3DXVECTOR2 start_pos, credit_pos, exit_pos, vect_pos, select_pos;
+int tCount, select_i;
 
 TitleScene::TitleScene(){
 
@@ -20,6 +22,18 @@ TitleScene::TitleScene(){
 	Graphic::Camera::Update ();
 
 	sprite.Create(L"Resource/Texture/Test.png");
+	sStart.Create(L"Resource/Texture/Start.png");
+	sCredit.Create(L"Resource/Texture/Credit.png");
+	sExit.Create(L"Resource/Texture/Exit.png");
+	sVector.Create(L"Resource/Texture/Vector.png");
+
+	start_pos = D3DXVECTOR2(800, 200);
+	credit_pos = D3DXVECTOR2(800, 400);
+	exit_pos = D3DXVECTOR2(800, 600);
+	vect_pos = D3DXVECTOR2(600, 200);
+	select_pos = D3DXVECTOR2(100, 0);
+	tCount = 0;
+	select_i = 0;
 }
 
 SCENE_STATUS TitleScene::Execute(){
@@ -32,9 +46,47 @@ SCENE_STATUS TitleScene::Execute(){
 		return END_PROCESS;
 	}
 
+	if (GamePad::getGamePadState(PAD_1, BUTTON_DOWN, 0) == INPUT_PUSH ||
+#ifdef _DEBUG
+		Keyboard::CheckKey(KC_DOWN) == INPUT_PUSH) {
+#endif
+		select_i++;
+	}
+	if (GamePad::getGamePadState(PAD_1, BUTTON_UP, 0) == INPUT_PUSH ||
+#ifdef _DEBUG
+		Keyboard::CheckKey(KC_UP) == INPUT_PUSH) {
+#endif
+		select_i--;
+	}
+
+	if (select_i < 0)
+		select_i = 2;
+	else if (select_i >= 3)
+		select_i = 0;
+
 	return STILL_PROCESSING;
 }
 
 void TitleScene::Draw(){
 	sprite.Draw();
+	sStart.SetPosition(start_pos);
+	sCredit.SetPosition(credit_pos);
+	sExit.SetPosition(exit_pos);
+	sVector.SetPosition(vect_pos);
+	switch (select_i){
+	case 0:
+		sStart.SetPosition(start_pos - select_pos);
+		break;
+	case 1:
+		sCredit.SetPosition(credit_pos - select_pos);
+		break;
+	case 2:
+		sExit.SetPosition(exit_pos - select_pos);
+		break;
+	}
+	sStart.Draw();
+	sCredit.Draw();
+	sExit.Draw();
+	sVector.SetPositionY(vect_pos.y + select_i * 200);
+	sVector.Draw();
 }
