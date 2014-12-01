6556b6e9f4b59e2def87a3f1774d94c9ac0e9b68
zxcvbnmmz <th.023102@gmail.com>
2014-12-01 11:40:08 +0900

    AnimationManager修正

---
+++ GraDeath/Include/Manager/AnimationManager.h
@@ -0,0 +1,46 @@
+#ifndef _ANIMATION_MANAGER_H__
+#define _ANIMATION_MANAGER_H__
+
+#include <D2D1.h>
+#include "Object/ObjectParameter.h"
+
+
+enum PLAYER_ACTION{
+	ACTION_IDLE,
+	ACTION_WALK,
+	ACTION_RUN,
+	ACTION_ATTACK,
+};
+
+enum CURRENT_STATE{
+	PROCESSED,
+	FINISHED,
+};
+
+class AnimationManager{
+public:
+	AnimationManager(){}
+	virtual ~AnimationManager(){}
+
+	bool Create(const char* fileName);
+
+	void ChangeAction(PLAYER_ACTION _action, bool _loop);
+
+	CURRENT_STATE Update();
+
+	void GetDrawingRect(D2D1_RECT_F& rect);
+
+	void Enable(bool flag){
+		isEnable = flag;
+	}
+
+private:
+	std::vector<std::shared_ptr<CellData>>::iterator currentCell;
+	PLAYER_ACTION currentAction;
+	unsigned int currentFrame;
+	AnimationData animation;
+	bool isEnable;
+	bool loop;
+};
+
+#endif	// end of AnimationManager
