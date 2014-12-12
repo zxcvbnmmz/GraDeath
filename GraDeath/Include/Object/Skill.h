04cf6b911db2fde58f62ab0a059219c700843ce7
Siarli <enter.528@gmail.com>
2014-12-12 10:24:31 +0900

    スキル実装中_一度更新

--- GraDeath/Include/Object/Skill.h
+++ GraDeath/Include/Object/Skill.h
@@ -3,30 +3,40 @@
 
 #include "Object/SkillDefine.h"
 #include "CharacterController/CharacterInfo.h"
+#include "Object/ObjectParameter.h"
 #include <memory>
+#include <vector>
 
 class Sprite;
 struct D3DXVECTOR2;
 struct AnimationData;
+struct CellData;
 
 class Skill
 {
 private:
-	Sprite* skillAnim;
-	std::shared_ptr< AnimationData > animSkill;
+	std::shared_ptr< Sprite > skillAnim;
+	AnimationData animSkill;
+	int frameCount = 0;
+	int animeMax = 0;
+	int nowAnime = 0;
+	D3DXVECTOR2 pos;
+	std::vector<std::shared_ptr<CellData>>::iterator currentCell;
 
 public:
 	Skill (){}
 
 	~Skill (){}
 
-	void Init ( wchar_t* _filename );
+	void Init ( char* _filename );
 
 	void Update ();
 
 	void Draw ();
 
 	void SetPosition ( const D3DXVECTOR2& _pos );
+
+	AnimationData& GetAnimationData ();
 
 };
 
