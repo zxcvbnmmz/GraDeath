319a67ebef7a464f13b3e7cbb183ddb58e2e6f65
Siarli <enter.528@gmail.com>
2014-12-15 11:31:29 +0900

    読込修正

--- GraDeath/Source/Loader/PlayerLoader.cpp
+++ GraDeath/Source/Loader/PlayerLoader.cpp
@@ -124,7 +124,6 @@ namespace PlayerLoader
 				ifs.read ( ( char* )&temp, sizeof( char ) );
 				cell->shapeCount = static_cast< short >( temp );
 
-				//if ( ( cell->shapeCount != 1 ) && ( _type == _PLAYER_TYPE) )
 				for ( int k = 0; k < cell->shapeCount; k++ )
 				{
 					auto shape = std::shared_ptr<ShapeData> ( new ShapeData );
