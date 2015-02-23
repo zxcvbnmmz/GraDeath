28d1ef178eafb468eea8f6d32313275fdd572fe9
Siarli <enter.528@gmail.com>
2015-02-23 14:00:22 +0900

    スキル１～３の追加

--- GraDeath/Source/CharacterController/CharacterController.cpp
+++ GraDeath/Source/CharacterController/CharacterController.cpp
@@ -4,8 +4,10 @@ CharacterController::CharacterController() :currentAction(ACTION_IDLE), enable(f
 	Actions[ACTION_WALK] = &CharacterController::Walk;
 	Actions[ACTION_JUMP_RISE] = &CharacterController::Jump_Rise;
 	Actions[ACTION_JUMP_FALL] = &CharacterController::Jump_Fall;
-	Actions[ACTION_JUMP_LAND] = &CharacterController::Jump_Land;
-	Actions[ACTION_SKILL] = &CharacterController::Skill;
+	Actions[ ACTION_JUMP_LAND ] = &CharacterController::Jump_Land;
+	Actions[ ACTION_SKILL ] = &CharacterController::Skill;
+	Actions[ ACTION_SKILL_2 ] = &CharacterController::Skill;
+	Actions[ ACTION_SKILL_3 ] = &CharacterController::Skill;
 	Actions[ACTION_IDLE] = &CharacterController::Idle;
 }
 
