53620b99a5b5553d9f92390b1dcb24754834d6b3
zxcvbnmmz <th.023102@gmail.com>
2015-02-25 15:17:23 +0900

    BGMマネージャー

---
+++ GraDeath/Include/Manager/BGMManager.h
@@ -0,0 +1,36 @@
+#ifndef _BGM_MANAGER_H__
+#define _BGM_MANAGER_H__
+
+#include <map>
+#include "Sound/BGM/BGM.h"
+
+class BGMManager{
+private:
+	BGMManager(){}
+	BGMManager(const BGMManager&);
+	BGMManager& operator=(const BGMManager&);
+
+public:
+	~BGMManager();
+
+	static BGMManager* GetInstance(){
+		if (inst == nullptr){
+			inst = new BGMManager;
+		}
+		return inst;
+	}
+
+	static void Release();
+
+	void Load(char* fileName);
+
+	void Play(char* fileName);
+	void Stop(char* fileName);
+
+
+private:
+	static BGMManager* inst;
+	std::map<char*, BGM> BGMs;
+};
+
+#endif	// end of BGMManager
